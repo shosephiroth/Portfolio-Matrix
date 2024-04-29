352a9d953b97257904da97d2c1daa9918023e39d
Shosephiroth <44537080+shosephiroth@users.noreply.github.com>
2024-04-29 11:46:15 -0400

    Update script.js
    change the text color and comment on the same line

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
             ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
             ctx.fillRect(0, 0, c.width, c.height);
 
-            ctx.fillStyle = "#f4427d";//green text
+            ctx.fillStyle = "#0000FF";//blue text
             ctx.font = font_size + "px arial";
             //looping over drops
             for(var i = 0; i < drops.length; i++)
