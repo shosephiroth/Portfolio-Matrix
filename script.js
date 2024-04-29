ca040291da715af2c94e2d42f11aa86a64baf7e6
Shosephiroth <44537080+shosephiroth@users.noreply.github.com>
2024-04-29 12:22:17 -0400

    Update script.js
    changed back to green text

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
             ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
             ctx.fillRect(0, 0, c.width, c.height);
 
-            ctx.fillStyle = "#0000FF";//blue text
+            ctx.fillStyle = "#39d106";//green text
             ctx.font = font_size + "px arial";
             //looping over drops
             for(var i = 0; i < drops.length; i++)
